dc8b82dafea4f10cdf3ad45d58031999e53c0d0f
Victor Palade <victor@cloudflavor.io>
2024-08-06 23:34:18 +0200

    chore: add bulma and deps.
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

---
+++ src/components/navbar.svelte
@@ -0,0 +1,48 @@
+<nav class="navbar main-navbar is-fixed-top" aria-label="main navigation">
+	<div class="container">
+		<div class="navbar-brand">
+			<a class="navbar-item is-size-4" href="/">
+				<img class="mr-4" src="/images/skycrane.svg" alt="Skycrane logo" />
+				<span>Skycrane</span>
+			</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a
+				role="button"
+				class="navbar-burger"
+				aria-label="menu"
+				aria-expanded="false"
+				data-target="navMenu"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+			</a>
+		</div>
+		<div id="navMenu" class="navbar-menu ml-6">
+			<div class="navbar-start">
+				<a class="navbar-item is-size-5" href="/overview"> Overview </a>
+				<a class="navbar-item is-size-5" href="https://docs.skycrane.io"> Docs </a>
+				<div class="navbar-item has-dropdown is-hoverable is-size-5">
+					<a class="navbar-link" href="/skyforge"> Skyforge </a>
+					<div class="navbar-dropdown">
+						<a class="navbar-item" href="/skyforge#registry"> Registry </a>
+						<a class="navbar-item" href="/docs.skycrane.io"> Docs </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="navbar-end">
+		<div class="navbar-item">
+			<div class="buttons">
+				<span class="icon">
+					<a href="https://www.github.com/cloudflavor/skycrane">
+						<i class="fa-brands fa-github fa-2xl"></i>
+					</a>
+				</span>
+			</div>
+		</div>
+	</div>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Navbar from '../components/navbar.svelte';
+
+	onMount(() => {
+		document.title = 'Skycrane';
+		document.documentElement.setAttribute('data-theme', 'light');
+	});
+</script>
+
+<div class="main-container">
+	<Navbar />
+	<slot />
+</div>
+
+<style lang="scss">
+	@import '/styles/global.scss';
+</style>
