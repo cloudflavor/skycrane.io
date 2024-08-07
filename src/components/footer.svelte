46d74f054390899203a9501b0dd5b8d687b682e9
Victor Palade <victor@cloudflavor.io>
2024-08-07 18:14:55 +0200

    web: add landing page basics.
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

---
+++ src/components/footer.svelte
@@ -0,0 +1,11 @@
+<footer class="footer mt-6">
+	<div class="content has-text-centered">
+		<p>
+			<strong>Skycrane</strong> by <a href="https://cloudflavor.io">Cloudflavor</a>. The source code
+			is licensed
+			<a href="https://www.apache.org/licenses/LICENSE-2.0">Apache 2.0</a>. The website content is
+			licensed
+			<a href="https://creativecommons.org/licenses/by-nc-sa/4.0//">CC BY NC SA 4.0</a>.
+		</p>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+
 	import Navbar from '../components/navbar.svelte';
+	import Footer from '../components/footer.svelte';
 
 	onMount(() => {
 		document.title = 'Skycrane';
@@ -9,8 +11,25 @@
 </script>
 
 <div class="main-container">
-	<Navbar />
-	<slot />
+	<div class="area">
+		<ul class="circles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<Navbar />
+		<slot />
+		<Footer />
+	</div>
 </div>
 
 <style lang="scss">
